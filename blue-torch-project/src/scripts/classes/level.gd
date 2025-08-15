f4f533df7e3c07b7b2a0d85eee8c5d22512531be
Moortin35 <moortindelgado@gmail.com>
2025-08-15 15:39:01 -0300

    Cambios menores, se agrego jugo un poco con las capas de los tilesets

--- blue-torch-project/src/scripts/classes/level.gd
+++ blue-torch-project/src/scripts/classes/level.gd
@@ -3,8 +3,10 @@ extends Node
 class_name Level
 
 @export var level_id : int
+@onready var torch: AnimatedSprite2D = $torch
 
 var level_data : LevelData
 
 func _ready() -> void:
 	level_data = LevelManager.get_level_by_id(level_id)
+	torch.play()
