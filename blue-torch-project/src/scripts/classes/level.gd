be6a86b221e6ace04f29ff96e113b4f20869bcc9
Miloro <miloromiguel@gmail.com>
2025-08-16 20:07:24 -0300

    fix luz

--- blue-torch-project/src/scripts/classes/level.gd
+++ blue-torch-project/src/scripts/classes/level.gd
@@ -3,10 +3,9 @@ extends Node
 class_name Level
 
 @export var level_id : int
-@onready var torch: AnimatedSprite2D = $torch
+
 
 var level_data : LevelData
 
 func _ready() -> void:
 	level_data = LevelManager.get_level_by_id(level_id)
-	torch.play()
