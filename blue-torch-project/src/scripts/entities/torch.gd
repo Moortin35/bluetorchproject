cb7c76badc1e2604be4271c20eea67891663b08f
Moortin35 <moortindelgado@gmail.com>
2025-08-16 20:45:51 -0300

    minimos cambios en antorchas

---
+++ blue-torch-project/src/scripts/entities/torch.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var torch: AnimatedSprite2D = $torch
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	torch.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
