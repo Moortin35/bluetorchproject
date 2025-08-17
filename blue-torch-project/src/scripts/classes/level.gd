51a22fd7f31eec33dbee84697d0e1e0b9069eaef
Moortin35 <moortindelgado@gmail.com>
2025-08-17 17:27:20 -0300

    Cambios sutiles en el orden de las carpetas y scripts

--- blue-torch-project/src/scripts/classes/level.gd
+++ blue-torch-project/src/scripts/classes/level.gd
@@ -9,3 +9,7 @@ var level_data : LevelData
 
 func _ready() -> void:
 	level_data = LevelManager.get_level_by_id(level_id)
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("salir_menu"):
+		get_tree().reload_current_scene()
