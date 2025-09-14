78a1b7695d88b4f52102ab4ee32988a4cc438cac
Moortin <moortindelgado@gmail.com>
2025-09-13 21:22:22 -0300

    avances con el nodo de avanzar de nivel
    se implemento un nodo que sirve como interacción para avanzar de nivel, todavía no finalizado.

--- blue-torch-project/src/scripts/ui/hud.gd
+++ blue-torch-project/src/scripts/ui/hud.gd
@@ -1,5 +1,9 @@
 extends CanvasLayer
 
+@onready var margin_container: MarginContainer = %MarginContainer
 
 func _on_exit_button_pressed() -> void:
 	get_tree().reload_current_scene()
+
+func mostrar_menu():
+	margin_container.show()
