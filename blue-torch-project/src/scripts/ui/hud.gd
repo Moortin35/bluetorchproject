3ffa0ba585e8c46b09e7fb082a640459a42bf49a
Moortin <moortindelgado@gmail.com>
2025-09-19 21:31:41 -0300

    Se implemento una base de caja de texto con retrato
    Se implemento una base de caja de texto con retrato

--- blue-torch-project/src/scripts/ui/hud.gd
+++ blue-torch-project/src/scripts/ui/hud.gd
@@ -2,8 +2,13 @@ extends CanvasLayer
 
 @onready var margin_container: MarginContainer = %MarginContainer
 
+
 func _on_exit_button_pressed() -> void:
 	get_tree().reload_current_scene()
 
 func mostrar_menu():
 	margin_container.show()
+	set_process(false)
+	set_process_unhandled_input(false)
+	set_process_input(false)
+	set_physics_process(false)
