9587f8cbb06b389d64bad4cf7f1024ac1edccd6f
Moortin <moortindelgado@gmail.com>
2025-11-05 18:04:21 -0300

    Cambio de accion del boton de finalizar nivel

--- blue-torch-project/src/scripts/ui/end_test.gd
+++ blue-torch-project/src/scripts/ui/end_test.gd
@@ -5,7 +5,9 @@ extends CanvasLayer
 
 func _on_exit_button_pressed() -> void:
 	get_tree().paused = false
-	get_tree().reload_current_scene()
+	LevelManager.load_level(2)
+	AudioControler.stop_music()
+	AudioControler.play_lvl1()
 
 func mostrar_menu():
 	margin_container.show()
