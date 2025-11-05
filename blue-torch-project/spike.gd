ccdb33169a44124ed4e038624849c834b9f82bc5
Miloro <miloromiguel@gmail.com>
2025-11-05 20:58:18 -0300

    se arregla el codigo de los spikes, ahora se ponen de a uno en el mapa

--- blue-torch-project/spike.gd
+++ blue-torch-project/spike.gd
@@ -1,30 +1,17 @@
 extends Node2D
 class_name Spike
 
-@onready var sprite_2d: Sprite2D = $Sprite2D
-@onready var collision_shape_2d: CollisionShape2D = $DangerZone/CollisionShape2D
-@export var size: int = 1
+@onready var sprite: Sprite2D = $sprite
+@export var nro_spike: int = 0
 
 func _ready() -> void:
-	randomize()
-
-	sprite_2d.hframes = 6
-	sprite_2d.vframes = 1
-
-	# Duplicar los sprites según el tamaño
-	for i in range(size):
-		var sprite: Sprite2D = sprite_2d.duplicate()
-		sprite.frame = randi_range(0, (sprite_2d.hframes * sprite_2d.vframes) - 1)
-		sprite.position.x = 16 * i
-		add_child(sprite)
-
-	# Asegurar que la forma de colisión sea única para este nodo
-	if collision_shape_2d.shape:
-		var shape = collision_shape_2d.shape.duplicate()
-		if shape is RectangleShape2D:
-			shape.size.x = 16 * size
-		collision_shape_2d.shape = shape
-
-	# Reposicionar el CollisionShape2D al centro del conjunto de spikes
-	collision_shape_2d.position.x = (collision_shape_2d.shape.size.x / 2) - 8
-		
+	var spike_width = 16
+	var spike_height = 16
+	var base_x = 48
+	var base_y = 272
+	
+	if(!(nro_spike > 0 and nro_spike < 7)):
+		nro_spike = randi_range(1, 6)
+	var rect_x = base_x + (nro_spike * spike_width)
+	var rect = Rect2(rect_x, base_y, spike_width, spike_height)
+	sprite.region_rect = rect
