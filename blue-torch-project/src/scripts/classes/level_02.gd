0a923d293e1939e83e20c6df1ba7157e6ed0f4b3
Moortin <moortindelgado@gmail.com>
2025-11-06 12:11:06 -0300

    Corrección de parallax y limites de nivel 2

--- blue-torch-project/src/scripts/classes/level_02.gd
+++ blue-torch-project/src/scripts/classes/level_02.gd
@@ -3,6 +3,6 @@ extends Level
 func _ready() -> void:
 	level_data = LevelManager.get_level_by_id(level_id)
 	player.camera_2d.limit_right = 1792
-	player.camera_2d.limit_bottom =704
+	player.camera_2d.limit_bottom =736
 	player.camera_2d.limit_left = -1440
 	player.camera_2d.limit_top = -240
