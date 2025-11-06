b0d3cf9ca27449ce149bae40b7c25842e13315a6
Moortin <moortindelgado@gmail.com>
2025-11-06 11:30:12 -0300

    Se definieron los limites de la camara del nivel 2

--- blue-torch-project/src/scripts/classes/level_02.gd
+++ blue-torch-project/src/scripts/classes/level_02.gd
@@ -2,7 +2,7 @@ extends Level
 
 func _ready() -> void:
 	level_data = LevelManager.get_level_by_id(level_id)
-	player.camera_2d.limit_right = 3000
-	player.camera_2d.limit_bottom =3000
-	player.camera_2d.limit_left = -3000
-	player.camera_2d.limit_top = -3000
+	player.camera_2d.limit_right = 1792
+	player.camera_2d.limit_bottom =704
+	player.camera_2d.limit_left = -1440
+	player.camera_2d.limit_top = -240
