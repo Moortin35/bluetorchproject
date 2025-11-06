3728bb11e1f225bcea70467e93879e81109b43fe
Miloro <miloromiguel@gmail.com>
2025-11-06 16:36:21 -0300

    se puede interactuar con el npc

---
+++ blue-torch-project/src/scenes/entities/npc.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _ready() -> void:
+	animated_sprite_2d.play("idle")
+
+func _process(_delta: float) -> void:
+	pass
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		print("holis")
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body is Player:
+		print("chauchis")
