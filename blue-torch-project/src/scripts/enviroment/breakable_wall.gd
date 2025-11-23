dab10f8bb7c1413e2824617784de8f150d163bb2
Moortin <moortindelgado@gmail.com>
2025-11-22 21:59:04 -0300

    Implementación del muro

---
+++ blue-torch-project/src/scripts/enviroment/breakable_wall.gd
@@ -0,0 +1,34 @@
+extends interactive
+
+@export var broken : bool = false
+@onready var wall_sprites: AnimatedSprite2D = $WallSprites
+@onready var collision_shape_default: CollisionShape2D = $StaticBody2D/CollisionShapeDefault
+@onready var collision_shape_collapsed: CollisionShape2D = $StaticBody2D/CollisionShapeCollapsed
+@onready var debris_sprite: Sprite2D = $DebrisSprite
+@onready var interactive_area: CollisionShape2D = $InteractiveArea
+
+func _ready():
+	super._ready()
+	icon = "hand"
+	if broken:
+		broken_wall()
+	else:
+		unbroken_wall()
+
+
+func _process(_delta: float) -> void:
+	if can_i_interact() and player != null: 
+			broken_wall()
+
+			
+func broken_wall():
+	wall_sprites.play("collapsed")
+	interactive_area.disabled = true
+	collision_shape_default.disabled = true
+	debris_sprite.show()
+	
+func unbroken_wall():
+	wall_sprites.play("default")
+	interactive_area.disabled = false
+	collision_shape_default.disabled = false
+	debris_sprite.hide()
