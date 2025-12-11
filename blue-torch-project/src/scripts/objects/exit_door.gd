787a28e0834bad7d14a5f12184665a4e0f5dee0e
Miloro <miloromiguel@gmail.com>
2025-12-11 13:11:57 -0300

    se agrega musica final cuando se gana el lvl

--- blue-torch-project/src/scripts/objects/exit_door.gd
+++ blue-torch-project/src/scripts/objects/exit_door.gd
@@ -2,7 +2,7 @@ extends interactive
 
 @onready var next_level: CanvasLayer = $NextLevel
 @export var level_to_pass: int
-
+const END_LEVEL_MUSIC = preload("res://_assets/sounds/music/end_level_music.wav")
 func _ready() -> void:
 	super._ready()
 	icon = "interact"
@@ -10,4 +10,5 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if can_i_interact():
 		next_level.show()
-		get_tree().paused = true
+		AudioControler.play_lvl_music(END_LEVEL_MUSIC)
+		player.can_control = false
